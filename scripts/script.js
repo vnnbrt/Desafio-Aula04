5a12d252bc5b8639df8c161108adbf2529f3b231
vnnbrt <vinniebritos@gmail.com>
2024-07-30 17:36:16 -0300

    ajustes no form html, estilização da pagina e melhorias no código JS

--- scripts/script.js
+++ scripts/script.js
@@ -4,26 +4,32 @@ function calcularIMC(peso, altura){
 
 function classificarIMC(imc){
     if (imc < 18.5){
-        return "Abaixo do peso";
+        return "um peso abaixo do ideal. Recomenda-se consultar um nutricionista para desenvolver um plano alimentar que ajude a alcançar um peso saudável.";
     } else if (imc >=18.5 && imc < 24.9) {
-        return "Peso adequado";
+        return "que está dentro da faixa ideal. É importante continuar com uma alimentação equilibrada e hábitos saudáveis para manter o bem-estar.";
     } else if (imc >= 25 && imc < 29.9) {
-        return "Sobrepeso";
+        return "sobrepeso. Considera-se falar com um especialista em nutrição para ajustar a dieta e incorporar exercícios físicos para alcançar um peso saudável.";
     } else {
-        return "Obesidade";
+        return "obesidade. Procurar ajuda médica é importante para gerenciar a obesidade de forma eficaz e garantir um plano de tratamento adequado às necessidades.";
     }
 }
 
-function sugerirAcao(classificao){
-    if (classificao === "Abaixo do peso") {
-        return "Considere consultar um nutricionista para ajudar a desenvolver um plano alimentar que ajude a alcançar um peso saudável.";
-    } else if (classificao === "Peso adequado") {
-        return "Seu peso está dentro da faixa ideal. Continue com uma alimentação equilibrada e hábitos saudáveis para manter seu bem-estar.";
-    } else if (classificao === "Sobrepeso") {
-        return "Considere falar com um especialista em nutrição para ajustar sua dieta e incorporar exercícios físicos para alcançar um peso saudável.";
-    } else if (classificao === "Obesidade") {
-        return "Procurar ajuda médica é importante para gerenciar a obesidade de forma eficaz e garantir um plano de tratamento adequado às suas necessidades.";
-    } else {
-        return "Classificação desconhecida.";
+function processarIMC(event) {
+    event.preventDefault();
+
+    const peso = parseFloat(document.getElementById("peso").value);
+    const altura = parseFloat(document.getElementById("altura").value);
+
+    if (isNaN(peso) || isNaN(altura) || peso <=0 || altura <=0){
+        document.getElementById("resultado").innerText = "Por favor, insira valores válidos para peso e altura."
+        console.log("Erro: Valores inválidos.")
+        return;
     }
+    
+    const imc = calcularIMC(peso , altura);
+    const classificar = classificarIMC(imc);
+
+    const resultadoTexto = `O IMC calculado é ${imc.toFixed(2)} e indica ${classificar}`;
+    document.getElementById("resultado").innerText = resultadoTexto;
+    console.log(resultadoTexto);
 }
